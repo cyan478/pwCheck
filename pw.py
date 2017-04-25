38967a87c981a9b0d47409dc31abbdf207b8bc6c
cyan478 <cyan2@stuy.edu>
2017-04-24 23:11:16 -0400

    did #1

--- pw.py
+++ pw.py
@@ -1,19 +1,5 @@
 def pwCheck(pw):
-	List = [ord(i) for i in pw]
-	cap = false
-	low = false
-	num = false
-	for a in List:
-		if a >= 65  and a <= 90:
-			cap = true
-		if a >= 97  and a <= 122:
-			low = true
-		if a >= 48  and a <= 57:
-			num = true
-	if cap and low and num:
-		print "Passed"
-		return true
-	else:
-		print "Failed"
-		return false	
-
+	cap = [i for i in pw if i.isupper()]
+	low = [i for i in pw if i.islower()]
+	num = [i for i in pw if i.isdigit()]
+	return len(cap) > 0 and len(low) > 0 and len(num) > 0
